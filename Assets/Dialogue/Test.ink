8a3931cd0e6d349125ac8540f989f9136952d1d7
zy-rex <zyrexx98@gmail.com>
2022-03-28 10:32:55 +0100

    Choices implemented, portraits nearly done

--- Assets/Dialogue/Test.ink
+++ Assets/Dialogue/Test.ink
@@ -1,2 +1,22 @@
-Black Hat Pirates
-Don't slip up
+-> TestingKnot
+
+
+
+
+=== TestingKnot
+HEALING: He tried that. #portrait:healing #speaker:healing
+LIEDSON: I bang out fifa daily. #portrait:liedson #speaker:liedson
+* Agree with Liedson
+    -> TestingKnot3
+* Agree with healing
+    -> TestingKnot2
+* [Try it this example!]
+    Mazza. #portrait:taj #speaker:taj
+-> END
+=== TestingKnot2
+aaaaaa #speaker:liedson #speaker:liedson
+-> END
+=== TestingKnot3
+Vegeta is hench #speaker:healing #speaker:healing
+
+-> END
--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Ink.Runtime;
+using UnityEngine.EventSystems;
 
 
 public class DialogueManager : MonoBehaviour
@@ -12,10 +13,18 @@ public class DialogueManager : MonoBehaviour
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI displayNameText;
     [SerializeField] Story currentStory;
 
 
+    [Header("Choices UI")]
+    [SerializeField] GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+
     public bool dialogueIsPlaying;
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
     
 
 
@@ -36,6 +45,14 @@ public class DialogueManager : MonoBehaviour
     private void Start()
     {
         dialogueIsPlaying = false;
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     private void Update()
@@ -67,6 +84,7 @@ public class DialogueManager : MonoBehaviour
         dialogueIsPlaying = false;
         //remove dialogue ui
         dialogueText.text = "'";
+
     }
 
     private void ContinueStory()
@@ -74,10 +92,79 @@ public class DialogueManager : MonoBehaviour
         if (currentStory.canContinue)
         {
             dialogueText.text = currentStory.Continue();
+
+            DisplayChoices();
+            HandleTags(currentStory.currentTags);
         }
         else
         {
             ExitDialogueMode();
         }
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(':');
+            if(splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);
+            }
+
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey)
+            {
+            case SPEAKER_TAG:
+                displayNameText.text = tagValue;
+                break;
+            case PORTRAIT_TAG:
+                Debug.Log("portrait=" +tagValue);
+                break;
+            default:
+                Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+                break;
+            }
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        if(currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than the UI can support. Number of choices given: " + currentChoices.Count);
+        }
+
+        int index = 0;
+
+        foreach(Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
     }
 }
