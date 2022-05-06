6354a26f23dc379d903d61f5b9cf5722e76fea24
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2022-05-06 01:46:17 +0100

    Almost finished crossroad

--- Assets/Scripts/MoveCameraScript.cs
+++ Assets/Scripts/MoveCameraScript.cs
@@ -6,6 +6,8 @@ public class MoveCameraScript : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 cameraBoundary;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,7 @@ public class MoveCameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 offsetPlayerPosition = Vector3.Scale(player.position, transform.right) + Vector3.Scale(transform.position, transform.up + transform.forward);
-        transform.position = Vector3.Lerp(transform.position, offsetPlayerPosition, Time.deltaTime * speed);
+        Vector3 lerpValue = Vector3.Lerp(transform.position, player.position + offset, Time.deltaTime * speed);
+        transform.position = new Vector3(lerpValue.x, transform.position.y, lerpValue.z);
     }
 }
