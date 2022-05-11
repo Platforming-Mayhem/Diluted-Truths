5299a5856ce24ed5dbe66291016c30ad0bb7ddf8
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2022-05-11 10:39:14 +0100

    Working on moving lights at the same speed as the lamp texture

---
+++ Assets/Scripts/AnimatedMovementObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedMovementObject : MonoBehaviour
+{
+    [SerializeReference]
+    private int frameRate = 24;
+
+    [SerializeReference]
+    private int framesPerRow = 12;
+
+    Vector3 originalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("WaitToPresentNextFrame");
+        offsetAmount = 0.0f;
+        originalPosition = transform.position;
+    }
+
+
+    float offsetAmount;
+
+    IEnumerator WaitToPresentNextFrame()
+    {
+        for (; ; )
+        {
+            if (offsetAmount >= 1.0f)
+            {
+                offsetAmount = 0.0f;
+            }
+            yield return new WaitForSeconds((float)(1.0f / frameRate));
+            if (offsetAmount >= 1.0f)
+            {
+                offsetAmount = 0.0f;
+            }
+            else
+            {
+                offsetAmount += (float)(1.0f / framesPerRow);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(originalPosition.x - (offsetAmount * 22.12143f), transform.position.y, transform.position.z);
+    }
+}
